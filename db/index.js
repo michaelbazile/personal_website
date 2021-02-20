c634e4be41c91b777cbd6b4a535cf2415c1a5429
xoweekday <xoweekday@gmail.com>
2021-02-19 18:50:21 -0600

    set up intial db connection

---
+++ db/index.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const { Pool } = require("pg");
+
+const db = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+});
+
+db.connect()
+  .then(() => {
+    console.log(`connected to database`);
+  })
+  .catch((error) => {
+    console.log(`not connected due to error: ${error}`);
+  });
+
+module.exports = {
+  db,
+};
